f07204ad7ce2edcc9784701bfca944259d0099b2
9-Bogdan <127949078+9-Bogdan@users.noreply.github.com>
2024-06-20 21:34:45 +0300

    connect

--- database/connect_db.py
+++ database/connect_db.py
@@ -3,9 +3,14 @@ from sqlalchemy.orm import sessionmaker
 
 from conf.config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+# SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+POSTGRES_DB=settings.postgres
+POSTGRES_USER=vatrupzn
+POSTGRES_PASSWORD=pOyqg24qPVdWL0gaKD3lKgPhtcaM1AYR
+POSTGRES_PORT=5432
+POSTGRES_HOST=abul.db.elephantsql.com
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_engine(f"postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
